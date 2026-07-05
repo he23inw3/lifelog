52de4dbacbea558258d2fa8a41b584091d2d649e
he23inw3 <golden2finger@gmail.com>
2026-07-05 13:14:49 +0900

    first commit

--- app/lifelog/src/main/java/jp/he23inw3/asset/configuration/VertexAiProducer.java
+++ app/lifelog/src/main/java/jp/he23inw3/asset/configuration/VertexAiProducer.java
@@ -2,12 +2,28 @@ package jp.he23inw3.asset.configuration;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.genai.Client;
+import com.google.genai.types.Blob;
 import com.google.genai.types.Candidate;
+import com.google.genai.types.CitationMetadata;
+import com.google.genai.types.CodeExecutionResult;
 import com.google.genai.types.Content;
+import com.google.genai.types.ExecutableCode;
+import com.google.genai.types.FileData;
+import com.google.genai.types.FinishReason;
+import com.google.genai.types.FunctionCall;
+import com.google.genai.types.FunctionResponse;
 import com.google.genai.types.GenerateContentConfig;
+import com.google.genai.types.GenerateContentParameters;
 import com.google.genai.types.GenerateContentResponse;
+import com.google.genai.types.GenerateContentResponsePromptFeedback;
+import com.google.genai.types.GenerateContentResponseUsageMetadata;
+import com.google.genai.types.GroundingMetadata;
 import com.google.genai.types.Part;
+import com.google.genai.types.SafetyRating;
+import com.google.genai.types.SafetySetting;
 import com.google.genai.types.Schema;
+import com.google.genai.types.Tool;
+import com.google.genai.types.ToolConfig;
 import com.google.genai.types.Type;
 import com.google.genai.types.UsageMetadata;
 import io.quarkus.arc.profile.IfBuildProfile;
@@ -21,16 +37,40 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RegisterForReflection(
         targets = {
+                Blob.class,
+                Candidate.class,
+                Candidate.Builder.class,
+                CitationMetadata.class,
+                CodeExecutionResult.class,
+                Content.class,
+                Content.Builder.class,
+                ExecutableCode.class,
+                FileData.class,
+                FinishReason.class,
+                FinishReason.Known.class,
+                FunctionCall.class,
+                FunctionResponse.class,
                 GenerateContentConfig.class,
                 GenerateContentConfig.Builder.class,
+                GenerateContentParameters.class,
+                GenerateContentParameters.Builder.class,
                 GenerateContentResponse.class,
-                Candidate.class,
-                Content.class,
+                GenerateContentResponse.Builder.class,
+                GenerateContentResponsePromptFeedback.class,
+                GenerateContentResponseUsageMetadata.class,
+                GroundingMetadata.class,
                 Part.class,
-                UsageMetadata.class,
+                Part.Builder.class,
+                SafetyRating.class,
+                SafetySetting.class,
                 Schema.class,
                 Schema.Builder.class,
-                Type.class
+                Tool.class,
+                ToolConfig.class,
+                Type.class,
+                Type.Known.class,
+                UsageMetadata.class,
+                UsageMetadata.Builder.class
         })
 public class VertexAiProducer {
 
